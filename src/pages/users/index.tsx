5b3d4b48b8968bb6822ee6eb464651d5ddbe3e55
gabriel3421 <gabrielsouza.n.s@hotmail.com>
2021-06-15 16:51:54 -0300

    controle de usuarios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,6 +1,7 @@
 import { Box, Flex, Heading, Icon, Button, Table, Thead, Tr, Th, Checkbox,Text, Tbody, Td } from "@chakra-ui/react";
 import { RiAddLine } from "react-icons/ri";
 import Header from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
 import Sidebar from "../../components/Sidebar";
 
 
@@ -48,6 +49,7 @@ export default function UserList(){
               </Tr>
             </Tbody>
           </Table>
+          <Pagination />
         </Box>
       </Flex>
     </Box>
