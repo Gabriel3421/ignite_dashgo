8f0ea60c812f2f0293ca16b503a15264c2eb03c4
gabriel3421 <gabrielsouza.n.s@hotmail.com>
2021-06-17 13:11:46 -0300

    react query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,5 +1,6 @@
-import { Box, Flex, Heading, Icon, Button, Table, Thead, Tr, Th, Checkbox,Text, Tbody, Td, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Flex, Heading, Icon, Spinner, Button, Table, Thead, Tr, Th, Checkbox,Text, Tbody, Td, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
+import { useQuery } from "react-query";
 import { RiAddLine } from "react-icons/ri";
 import Header from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
@@ -7,6 +8,24 @@ import Sidebar from "../../components/Sidebar";
 
 
 export default function UserList(){
+  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json()
+    const users = data.users.map( user => {
+      console.log(user)
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.created_at).toLocaleDateString('pt-br', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        }),
+      }
+    })
+    return users
+  })
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
@@ -32,32 +51,52 @@ export default function UserList(){
             </Link>
             
           </Flex>
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4","6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuarios</Th>
-                {isWideVersion && <Th>Data de cadastro</Th>}
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme='pink' />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight='bold'>asd</Text>
-                    <Text fontSize='sm' color='gray.300'>asd</Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>asd</Td>}
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+          {isLoading ? (
+            <Flex justify='center'>
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify='center'>
+              <Text>Falha ao obter dados dos usuários</Text>
+            </Flex>
+          ): (
+            <>
+              <Table colorScheme='whiteAlpha'>
+                <Thead>
+                  <Tr>
+                    <Th px={['4', '4', '6']} color='gray.300' width='8'>
+                      <Checkbox colorScheme='pink' />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isWideVersion && <Th>Data de cadastro</Th>}
+                  </Tr>
+                </Thead>
+
+                <Tbody>
+                  {data.map((user) => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={['4', '4', '6']}>
+                          <Checkbox colorScheme='pink' />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Link href="/">
+                              <Text fontWeight='bold'>{user.name}</Text>
+                            </Link>
+                            <Text fontSize='sm' color='gray.300'>{user.email}</Text>
+                          </Box>
+                        </Td>
+                        {isWideVersion && <Td>{user.createdAt}</Td>}
+                      </Tr>
+                    )
+                  })}
+                </Tbody>
+              </Table>
+
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
